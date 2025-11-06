62b20461d547200bde1710c0b6592edc608f29bb
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-11-06 10:45:30 +0800

    feat(migrations): 重新组织文件分类表结构
    - 调整了 files、groups 和 tags 表的创建顺序
    - 将 tags 表的定义从原有位置移动至 groups 表之后

--- migrations_postgres/2024-10-01-193345_FileClassification/up.sql
+++ migrations_postgres/2024-10-01-193345_FileClassification/up.sql
@@ -1,12 +1,4 @@
 -- Your SQL goes here
-CREATE TABLE IF NOT EXISTS files (
-    id SERIAL PRIMARY KEY,
-    type TEXT NOT NULL,
-    path TEXT NOT NULL,
-    reference_count INTEGER NOT NULL DEFAULT 0,
-    group_id INTEGER NOT NULL REFERENCES groups(id)
-);
-
 CREATE TABLE IF NOT EXISTS groups (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
@@ -18,16 +10,24 @@ CREATE TABLE IF NOT EXISTS groups (
     modify_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS file_groups (
-    file_id INTEGER NOT NULL REFERENCES files(id),
-    group_id INTEGER NOT NULL REFERENCES groups(id),
-    PRIMARY KEY (file_id, group_id)
-);
-
 CREATE TABLE IF NOT EXISTS tags (
     id SERIAL PRIMARY KEY,
     reference_count INTEGER NOT NULL DEFAULT 0,
     name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS files (
+    id SERIAL PRIMARY KEY,
+    type TEXT NOT NULL,
+    path TEXT NOT NULL,
+    reference_count INTEGER NOT NULL DEFAULT 0,
+    group_id INTEGER NOT NULL REFERENCES groups(id)
+);
+
+CREATE TABLE IF NOT EXISTS file_groups (
+    file_id INTEGER NOT NULL REFERENCES files(id),
+    group_id INTEGER NOT NULL REFERENCES groups(id),
+    PRIMARY KEY (file_id, group_id)
 );
 
 CREATE TABLE IF NOT EXISTS group_tags (
